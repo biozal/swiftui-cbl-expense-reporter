3613215ffaff44b1025d403d894e678a55219f54
Aaron LaBeau <alabeau@gmail.com>
2023-06-25 22:47:00 -0500

    Initial Commit

---
+++ ExpenseReporter/ExpenseReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseReporterApp.swift
+//  ExpenseReporter
+//
+//  Created by Aaron LaBeau on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
